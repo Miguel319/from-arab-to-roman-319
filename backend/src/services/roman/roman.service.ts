4b877f9740faa7f60e440ba9366545b0bfb4a418
Miguel319 <miguelp90.319@gmail.com>
2021-06-13 12:38:20 -0400

    Refactored arabToRoman

--- backend/src/services/roman/roman.service.ts
+++ backend/src/services/roman/roman.service.ts
@@ -2,22 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RomanService {
+  private static readonly arabToRoman: Array<Array<any>> = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+  ];
+
   fromArab(arab: number): string {
-    const arabToRoman: Array<Array<any>> = [
-      [1000, 'M'],
-      [900, 'CM'],
-      [500, 'D'],
-      [400, 'CD'],
-      [100, 'C'],
-      [90, 'XC'],
-      [50, 'L'],
-      [40, 'XL'],
-      [10, 'X'],
-      [9, 'IX'],
-      [5, 'V'],
-      [4, 'IV'],
-      [1, 'I'],
-    ];
+    const arabToRoman: Array<Array<any>> = RomanService.arabToRoman;
 
     if (arab === 0) return '';
 
